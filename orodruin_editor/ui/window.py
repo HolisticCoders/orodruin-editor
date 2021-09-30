c1b4340bdc0b0e676aeac124842316a4c0006aa2
Loïc Pinsard <muream@gmail.com>
2021-09-30 21:37:17 +0200

    add basic export node button

--- orodruin_editor/ui/window.py
+++ orodruin_editor/ui/window.py
@@ -1,15 +1,20 @@
+import logging
 from dataclasses import dataclass, field
 from typing import Optional
 
+import orodruin.commands
 from orodruin.core import State
 from PySide2.QtCore import Qt
-from PySide2.QtWidgets import QDockWidget, QMainWindow, QWidget
+from PySide2.QtWidgets import QAction, QDockWidget, QMainWindow, QMenuBar, QWidget
 
 from orodruin_editor.ui.editor.graphics_state import GraphicsState
 
 from ..models.node_list_model import NodeListModel
+from .editor.graphics_items.graphics_node import GraphicsNode
 from .editor.graphics_view import GraphicsView
 from .node_list_view import NodeListView
+
+logger = logging.getLogger(__name__)
 
 
 @dataclass
@@ -19,11 +24,25 @@ class OrodruinWindow(QMainWindow):
 
     _graphics_state: GraphicsState = field(init=False)
 
+    _view: GraphicsView = field(init=False)
+    _menu_bar: QMenuBar = field(init=False)
+
+    _export_node_action: QAction = field(init=False)
+    _node_list_model: NodeListModel = field(init=False)
+    _node_list_view: NodeListView = field(init=False)
+
     def __post_init__(self) -> None:
         super().__init__(parent=self._parent)
 
         self.setWindowTitle("Orodruin Editor")
         self.setGeometry(200, 200, 1280, 720)
+
+        self._menu_bar = QMenuBar()
+        self.setMenuBar(self._menu_bar)
+        file_menu = self._menu_bar.addMenu("File")
+        self._export_node_action = QAction("Export Node")
+        file_menu.addAction(self._export_node_action)
+        self._export_node_action.triggered.connect(self._export_node)
 
         self._view = GraphicsView(self)
         self.setCentralWidget(self._view)
@@ -35,7 +54,25 @@ class OrodruinWindow(QMainWindow):
         dock.setAllowedAreas(Qt.LeftDockWidgetArea | Qt.RightDockWidgetArea)
         self.addDockWidget(Qt.LeftDockWidgetArea, dock)
 
-        node_list_model = NodeListModel()
-        node_list_view = NodeListView(self._graphics_state, dock)
-        node_list_view.setModel(node_list_model)
-        dock.setWidget(node_list_view)
+        self._node_list_model = NodeListModel()
+        self._node_list_view = NodeListView(self._graphics_state, dock)
+        self._node_list_view.setModel(self._node_list_model)
+        dock.setWidget(self._node_list_view)
+
+    def _export_node(self):
+        selection = self._view.scene().selectedItems()
+        if not selection:
+            logger.error("No node are selected.")
+            return
+
+        first_item = selection[0]
+        if not isinstance(first_item, GraphicsNode):
+            logger.error("Can't export a %s", type(first_item))
+            return
+
+        orodruin_node = self._state.node_from_nodelike(first_item.uuid())
+
+        orodruin.commands.ExportNode(
+            orodruin_node, "orodruin-library", "orodruin", orodruin_node.name()
+        ).do()
+        self._node_list_model.refresh_nodes_list()
